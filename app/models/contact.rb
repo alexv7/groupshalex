a000329c1ccec931c5dc9c872c660da0c5d9a3d5
alexv7 <alexvarjabedian7@gmail.com>
2016-05-20 13:51:38 -0400

    rails g migrate Contact name phone_number email message.....validations for them as well as pre-code for the form for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,3 @@
 class Contact < ActiveRecord::Base
+  validates :name, :phone_number, :message, :email, presence: true
 end
--- db/migrate/20160520164001_create_contacts.rb
+++ db/migrate/20160520164001_create_contacts.rb
@@ -1,6 +1,10 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :message
+      t.string :phone_number
 
       t.timestamps null: false
     end
